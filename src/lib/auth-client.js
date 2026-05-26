8c61d314ce433f5dab6223cf459f80bfde7fff0a
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-05-26 08:36:04 +0600

    fix authClient page

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 
 // 1. Create your configured client instance
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL // Note: In Next.js clientside, use NEXT_PUBLIC_BETTER_AUTH_URL if this value is needed directly on the client.
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL, // Note: In Next.js clientside, use NEXT_PUBLIC_BETTER_AUTH_URL if this value is needed directly on the client.
 })
 
 // 2. Extract the methods directly from your configured instance
